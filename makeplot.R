1e8f38d14fad35fb71da33ee1714c8f2a05ece88
glimmek2 <118555495+glimmek2@users.noreply.github.com>
2025-11-30 17:30:08 +0100

    Update makeplot.R
    added simulation scenarios, colors swapped

--- makeplot.R
+++ makeplot.R
@@ -1,12 +1,13 @@
-#makeplot.R: example call for rendering a PBR curve and the difference of 2 PBR curves
-#Example uses a study dataset from SAS
+# makeplot.R: example call for rendering a PBR curve and the difference of 2 PBR curves
+# Example uses REACH3 data 
 
 # Set the working directory to the directory of the this script
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("analysis_functions_sep_robustvar_fix.R")
 #read in data:
-eventsdata <- read_sas("eventsdata.sas7bdat") #must be in current working directory
+eventsdata <- read.csv("eventsdata.csv") #must be in current working directory
 colnames(eventsdata)<-c('ID','trt','time','trans02','cens0','trans01','trans12','cens1','totcases')
+
 
 #plot two curves with confidence bands
 output_BAT<-getPBIRcurve(eventsdata,group='Best available therapy')
@@ -16,19 +17,19 @@ output_RUX$trt<-'RUX10mgBID'
 output<-rbind(output_BAT,output_RUX)
 #output:
 # number of rows = number of unique event times (incl censoring times)
-#stoptime= event time
-#numtransij= number pats going from state i to state j at stoptime
-#censj= number pats being censored from state j immediately after stoptime
-#pbinj= percentage of patients in state j immediately after stoptime
+# stoptime= event time
+# numtransij= number pats going from state i to state j at stoptime
+# censj= number pats being censored from state j immediately after stoptime
+# pbinj= percentage of patients in state j immediately after stoptime
 
-plot(stepfun(output_RUX$stoptime,c(0,output_RUX$pbin1)),do.points=FALSE,col='red',
+plot(stepfun(output_RUX$stoptime,c(0,output_RUX$pbin1)),do.points=FALSE,col='blue',
      xlim=c(0,max(output$stoptime)+100),ylim=c(-0.1,1.1), main='probability of being in response',
      xlab='days',ylab='probability')
-lines(stepfun(output_BAT$stoptime,c(0,output_BAT$pbin1)),do.points=FALSE, col='blue')
-lines(stepfun(output_BAT$stoptime,c(0,output_BAT$lowlim)),do.points=FALSE, col='blue',lty='dotted')
-lines(stepfun(output_BAT$stoptime,c(0,output_BAT$uplim)),do.points=FALSE, col='blue',lty='dotted')
-lines(stepfun(output_RUX$stoptime,c(0,output_RUX$lowlim)),do.points=FALSE, col='red',lty='dotted')
-lines(stepfun(output_RUX$stoptime,c(0,output_RUX$uplim)),do.points=FALSE, col='red',lty='dotted')
+lines(stepfun(output_BAT$stoptime,c(0,output_BAT$pbin1)),do.points=FALSE, col='red')
+lines(stepfun(output_BAT$stoptime,c(0,output_BAT$lowlim)),do.points=FALSE, col='red',lty='dotted')
+lines(stepfun(output_BAT$stoptime,c(0,output_BAT$uplim)),do.points=FALSE, col='red',lty='dotted')
+lines(stepfun(output_RUX$stoptime,c(0,output_RUX$lowlim)),do.points=FALSE, col='blue',lty='dotted')
+lines(stepfun(output_RUX$stoptime,c(0,output_RUX$uplim)),do.points=FALSE, col='blue',lty='dotted')
 text(820,0.23,'BAT',cex=0.8)
 text(820,0.53,'RUX',cex=0.8)
 
@@ -38,9 +39,8 @@ diffcurve<-getDiffcurve(output_sorted)
 
 ylimlow<-min(diffcurve$lowlimDiff1)
 ylimup<-max(diffcurve$uplimDiff1)
-plot(stepfun(diffcurve$stoptime,c(0,diffcurve$p1Diff)),do.points=FALSE,col='red',
+plot(stepfun(diffcurve$stoptime,c(0,diffcurve$p1Diff)),do.points=FALSE,col='darkgrey',
      xlim=c(0,max(diffcurve$stoptime)+100),ylim=c(ylimlow-0.05,ylimup+0.05), main='difference of probability of being in response',
      xlab='days',ylab='probability')
-lines(stepfun(diffcurve$stoptime,c(0,diffcurve$lowlimDiff1)),do.points=FALSE, col='red',lty='dotted')
-lines(stepfun(diffcurve$stoptime,c(0,diffcurve$uplimDiff1)),do.points=FALSE, col='red',lty='dotted')
-
+lines(stepfun(diffcurve$stoptime,c(0,diffcurve$lowlimDiff1)),do.points=FALSE, col='darkgrey',lty='dotted')
+lines(stepfun(diffcurve$stoptime,c(0,diffcurve$uplimDiff1)),do.points=FALSE, col='darkgrey',lty='dotted')
